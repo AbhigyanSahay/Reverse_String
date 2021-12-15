77b6ffc21733a70043ef65e6158b75ebfabfe03f
AbhigyanSahay <94617422+AbhigyanSahay@users.noreply.github.com>
2021-12-15 19:21:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# This program will reverse the string entered by the user
+
+print("Welcome to the Reverse String program.")
+
+print("Enter the string: ")
+string = str(input())
+
+rev_string = string[::-1]
+
+print(f"The Reversed String is: \n{rev_string}")
